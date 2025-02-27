3b8318978c4e0f3b4f6afa7394f1c2b738d3abd3
Changoos <hgajghat@gmail.com>
2025-02-28 05:07:03 +0530

    no

--- discography/scripts.js
+++ discography/scripts.js
@@ -106,7 +106,7 @@ releaseType.textContent = `(${album.type})`;
 if (album.artist && (album.type.toLowerCase() === "feat" || album.type.toLowerCase() === "feature")) {
     artistAttribution.innerHTML = `by <a href="${album.artistLink || '#'}" target="_blank" style="color:rgb(229, 229, 229);">${album.artist}</a>`;
 } else {
-    artistAttribution.innerHTML = `by <span style="color: #FFD700;">changoos</span>`;
+    artistAttribution.innerHTML = `by <span style="color:rgb(227, 227, 227);">changoos</span>`;
 }
 
 
